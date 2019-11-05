89e84818231cdebda63c3d5e235fceb394d42c24
Shreyansh Deb <projects.shreyanshdeb@gmail.com>
2019-11-05 09:30:01 +0530

    Renamed files, fixed navigation

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function onFormSubmit(){
     xhr.onload = function(){
         if (xhr.status>=200 & xhr.status<300){
             console.log("success", xhr);
+            alert("Expense Added");
+            window.location.href="./index.html";
         }else{
             console.log("failed", xhr);
         }
@@ -42,11 +44,57 @@ function getExpenseList(){
 function populateList(json){
     const expenseList = document.querySelector("#expense-list-container > div");
 
+    while(expenseList.firstChild){
+        expenseList.firstChild.remove();
+    }
+ 
+    let category = "W";
+
     json.forEach((row)=>{   
-        row.forEach((cell)=>{
-            console.log(cell)
-        })
+        createCard(expenseList, category, row.amount, row.title, row.datetime);
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {getExpenseList()});
+function createCard(expenseList, category, amount, title, datetime){
+    const expenseCard = document.createElement('div');
+    const categoryContainer = document.createElement('div');
+    const expenseInfoContainer = document.createElement('div');
+    const expenseTitleConatiner = document.createElement('div');
+    const expenseAmount = document.createElement('label');
+    const expenseInfoTitle = document.createElement('label');
+    const expenseDatetimeContainer = document.createElement('div');
+    const expenseInfoDatetime = document.createElement('label');
+
+    expenseCard.className = "expense-card";
+    categoryContainer.className = "category-container";
+    expenseInfoContainer.className = "expense-info-container";
+    expenseTitleConatiner.className = "expense-tile-container";
+    expenseAmount.className = "expense-amount";
+    expenseInfoTitle.className = "expense-info-title";
+    expenseDatetimeContainer.className = "expense-datetime-container";
+    expenseInfoDatetime.className = "expense-info-datetime";
+
+    expenseCard.append(categoryContainer);
+    expenseCard.append(expenseInfoContainer);
+    expenseInfoContainer.append(expenseTitleConatiner);
+    expenseInfoContainer.append(expenseDatetimeContainer);
+    expenseTitleConatiner.append(expenseAmount);
+    expenseTitleConatiner.append(expenseInfoTitle);
+    expenseDatetimeContainer.append(expenseInfoDatetime);
+
+    categoryContainer.innerHTML = category;
+    expenseAmount.innerHTML = "₹ " + amount;
+    expenseInfoTitle.innerHTML = title;
+    expenseInfoDatetime.innerHTML = datetime;
+
+    expenseList.append(expenseCard);
+}
+
+function initDashboard(){
+    const btn = document.getElementById("newExpenseBtn");
+    btn.onclick = () => {
+        location.href = "./add_expense.html";
+    };
+
+    getExpenseList();
+}
